82f302af96b943d8f0d890bc09de0aa4d631820f
Kevin-Ratschinski <Kevin05774@hotmail.de>
2025-09-19 12:44:46 +0200

    feat: add VITE_MOCK_TEST environment variable and update type definitions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_MOCK_TEST: boolean;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
